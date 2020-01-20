4426dbb8b602728605cc2f288eb8de6cf8d983e6
Saumya Bhatnagar <saumya.bhatnagar@penske.com>
2020-01-19 21:46:44 -0500

    adding connection and other settings

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,47 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    let { id } =  req.params;
+
+    const item = await FeedItem.findByPk(id);
+    if ( !item ) {
+        res.status(404).send("record not found");
+    }
+    if(item.url) {
+        item.url = AWS.getGetSignedUrl(item.url);
+    }
+
+
+    
+    res.send(item);
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
-        //@TODO try it yourself
-        res.send(500).send("not implemented")
+    
+    let { id } =  req.params;
+    const caption = req.body.caption;
+    const fileName = req.body.url;
+    // check Caption is valid
+    const item = await FeedItem.findByPk(id);
+    if ( !item ) {
+        res.status(404).send("record not found");
+    }
+    
+    if (caption) {
+        item.caption = caption;
+
+    }
+
+    // check Filename is valid
+    if (fileName) {
+        item.url = fileName;
+    }
+    item.save();
+    res.send(201).send("update successful");
 });
 
 
