ed52843bbd6f1ef42b6d210b1498770088de67b7
Saumya Bhatnagar <saumya.bhatnagar@penske.com>
2020-01-25 16:08:34 -0500

    type: task
    Changes:
    Update the UI look and feel
    Updated the theme
    Updated the feed model in the frontend cod
    Opened CORS settings in backend to allow  call by frontend

--- course-02/exercises/udacity-c2-frontend/src/environments/environment.prod.ts
+++ course-02/exercises/udacity-c2-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://UdagramSaumyaDev-env.daqdb5g3zx.us-east-2.elasticbeanstalk.com/api/v0'
 };
 
 /*
